6cc97d914aba702c3c2132edab8f87f8634a2d81
cherro <adsl1664@gmail.com>
2020-12-31 16:42:57 +0900

    Revert "ignore 등록"
    This reverts commit 462dc631cfb0a35ce9419a6f0f15f4bed083c347.

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,15 +1,8 @@
 package com.jojoldu.book.springboot.web;
 
-import com.jojoldu.book.springboot.service.posts.PostsService;
-import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
-import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 /*
@@ -22,27 +15,14 @@ src/main/resources/templates/*.mustache
 @Controller
 public class IndexController {
 
-    private final PostsService postsService;
-
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("posts", postsService.findAllDesc());
+    public String index() {
         return "index";
     }
     // index가 자동으로 index.mustache로 전환되어 처리됨
 
-    // 게시글 저장
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
-    }
-
-    // 게시글 수정 이동
-    @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model) {
-        PostsResponseDto dto = postsService.findById(id);
-        model.addAttribute("post", dto);
-
-        return "posts-update";
     }
 }
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -4,16 +4,7 @@ let main = {
         $('#btn-save').on('click',function() {
             _this.save();
         });
-
-        $('#btn-update').on('click',function() {
-            _this.update();
-        });
-
-        $('#btn-delete').on('click',function() {
-            _this.delete();
-        })
     },
-
     save : function() {
         let data = {
             title: $('#title').val(),
@@ -31,44 +22,6 @@ let main = {
             alert('글이 등록되었습니다.');
             window.location.href = '/';
         }).fail(function(error) {
-            alert(JSON.stringify(error));
-        });
-    },
-
-    update : function() {
-        let data = {
-            title: $('#title').val(),
-            content: $('#content').val()
-        };
-
-        let id = $('#id').val();
-
-        $.ajax({
-            type: 'PUT',
-            url: '/api/v1/posts/'+id,   // 어느 게시글을 수정할지 url Path로 구분하기 위해 id 추가
-            dataType: 'json',
-            contentType: 'application/json; charset=utf-8',
-            data: JSON.stringify(data)
-        }).done(function() {
-            alert('글이 수정되었습니다.');
-            window.location.href = '/';
-        }).fail(function(error) {
-            alery(JSON.stringify(error));
-        });
-    },
-
-    delete : function() {
-        let id = $('#id').val();
-
-        $.ajax({
-            type: 'DELETE',
-            url: '/api/v1/posts/'+id,
-            dataType: 'json',
-            contentType: 'application/json; charset=utf-8'
-        }).done(function() {
-            alert('글이 삭제되었습니다.');
-            window.location.href='/';
-        }).fail(function (error) {
             alert(JSON.stringify(error));
         });
     }
