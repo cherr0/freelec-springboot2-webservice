462dc631cfb0a35ce9419a6f0f15f4bed083c347
cherro <adsl1664@gmail.com>
2020-12-31 16:41:55 +0900

    ignore 등록

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,8 +1,13 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // 보통 DAO라고 불리는 DB Layer 접근자
 public interface PostsRepository extends JpaRepository<Posts,Long> {
 
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,17 @@ package com.jojoldu.book.springboot.service.posts;
 
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,26 @@ public class PostsService {
                 .orElseThrow(() -> new
                         IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)         // .map(posts -> new PostsListResponseDto(posts)) 코드와 동일
+                .collect(Collectors.toList());          // 레포지토리 결과로 넘어온 Posts의 Stream을 map을 통해 Dto 변환 -> List로 반환하는 메소드
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new
+                        IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+        /*
+        JpaRepository에서 이미 delete 메소드를 지원하고 있으니 이를 활용한다.
+        엔티티를 파라미터로 삭제할 수도 있고, deleteById 메소드를 이용하면 id로 삭제 가능
+        존재하는 Posts인지 확인을 위해 엔티티 조회 후 그대로 삭제한다.
+         */
     }
 }
