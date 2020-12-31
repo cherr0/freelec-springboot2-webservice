0af6ffdf2fd63aec5a2e47f3249a1a8af2122eee
cherro <adsl1664@gmail.com>
2020-12-31 16:43:14 +0900

    Revert "Revert "ignore 등록""
    This reverts commit 6cc97d914aba702c3c2132edab8f87f8634a2d81.

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,8 +1,15 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.service.posts.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 /*
@@ -15,14 +22,27 @@ src/main/resources/templates/*.mustache
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
     // index가 자동으로 index.mustache로 전환되어 처리됨
 
+    // 게시글 저장
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    // 게시글 수정 이동
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
